1f1e0f9a898e9fdadd3f79886004283963973e97
Ali Cem GEZER <132958533+cemgezerr@users.noreply.github.com>
2023-08-14 05:29:33 +0300

    layout

---
+++ src/app/layout.jsx
@@ -0,0 +1,23 @@
+import Head from './head'
+import React from 'react'
+import './globals.css'
+import Header from '@/components/header'
+import Providers from './Providers'
+import Tabs from '@/components/Tabs'
+
+const Layout = ({ children }) => {
+    return (
+        <html lang='en'>
+            <Head />
+            <body>
+                <Providers>
+                <Header />
+                <Tabs />
+                {children}
+                </Providers>
+            </body>
+        </html>
+    )
+}
+
+export default Layout
