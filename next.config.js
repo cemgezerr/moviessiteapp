f5db2ecac345f9ee25b31ec5cf4653dd0bbc3443
Ali Cem GEZER <132958533+cemgezerr@users.noreply.github.com>
2023-08-14 05:28:49 +0300

    next image config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+    images : {
+      domains : ['image.tmdb.org', 'localhost', 'picsum.photos'] // <== Domain name
+    }
+  }
+  
+  module.exports = nextConfig
